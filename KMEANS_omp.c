17827bf5de832b56fe9baf4b547425b801b4f27c
ironmask2003 <114989576+ironmask2003@users.noreply.github.com>
2025-01-31 17:02:10 +0100

    OMP

--- KMEANS_omp.c
+++ KMEANS_omp.c
@@ -374,7 +374,7 @@ int main(int argc, char* argv[])
 		}
 		
 		maxDist=FLT_MIN;
-		#pragma omp parallel for private(distCentroids) reduction(max:maxDist)
+		#pragma omp parallel for reduction(max:maxDist)
 		for(i=0; i<K; i++){
 			distCentroids[i]=euclideanDistance(&centroids[i*samples], &auxCentroids[i*samples], samples);
 			if(distCentroids[i]>maxDist) {
