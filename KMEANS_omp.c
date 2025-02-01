7e983134c2021bc4c224202f4fa9136b30f52cbd
ironmask2003 <114989576+ironmask2003@users.noreply.github.com>
2025-02-01 18:20:52 +0100

    OMP

--- KMEANS_omp.c
+++ KMEANS_omp.c
@@ -394,7 +394,7 @@ int main(int argc, char* argv[])
 		}
 
 		maxDist=FLT_MIN;
-		#pragma omp for private(j, dist) reduction(max:maxDist)
+		#pragma omp parallel for private(j, dist) reduction(max:maxDist)
 		for(i=0; i<K; i++){
 			dist = 0.0;
 			for(j=0; j<samples; j++){
