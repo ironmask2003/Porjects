44dbeb473f7b6918cd0c6b0f0160e6f74be9a968
ironmask2003 <lachithaperera75@gmail.com>
2025-02-24 15:20:49 -0800

    Fixewd error

--- KMEANS_cudaf.cu
+++ KMEANS_cudaf.cu
@@ -273,8 +273,8 @@ __global__ void max(float* d_centroids, float* d_auxCentroids, float* d_maxDist,
 
     if (id < d_K){
         d_distCentroids[id]=euclideanDistance(&d_centroids[id*d_samples], &d_auxCentroids[id*d_samples], d_samples);
-        if(d_distCentroids[id]>d_maxDist) {
-            d_maxDist=d_distCentroids[id];
+        if(d_distCentroids[id]>*d_maxDist) {
+            *d_maxDist=d_distCentroids[id];
         }
     }
 }
