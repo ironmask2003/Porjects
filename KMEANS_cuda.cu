dc57f46e7d3a6437aa800d382d0baabe66632705
ironmask2003 <lachithaperera75@gmail.com>
2025-02-25 09:04:15 -0800

    1024 blocks

--- KMEANS_cuda.cu
+++ KMEANS_cuda.cu
@@ -444,7 +444,7 @@ int main(int argc, char* argv[])
     CHECK_CUDA_CALL( cudaMemcpy(d_auxCentroids, auxCentroids, K*samples*sizeof(float), cudaMemcpyHostToDevice) );
 
     // Set of the grid and block dimensions
-    dim3 blockSize(256);
+    dim3 blockSize(1024);
     dim3 numBlocks(ceil((static_cast<float>(lines) / blockSize.x)/2));
 
     printf("\nNumBlocks: %d\n", numBlocks.x);
