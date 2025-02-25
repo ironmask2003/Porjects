c4530feee31ad911a53cd1662417f4538261f861
ironmask2003 <lachithaperera75@gmail.com>
2025-02-25 09:10:35 -0800

    try

--- KMEANS_cuda.cu
+++ KMEANS_cuda.cu
@@ -445,7 +445,7 @@ int main(int argc, char* argv[])
 
     // Set of the grid and block dimensions
     dim3 blockSize(1024);
-    dim3 numBlocks(ceil(static_cast<float>(samples) / blockSize.x));
+    dim3 numBlocks(ceil(static_cast<float>(lines) / blockSize.x) + ceil(static_cast<float>(lines) / blockSize.x)/2);
 
     printf("\nNumBlocks: %d\n", numBlocks.x);
 
