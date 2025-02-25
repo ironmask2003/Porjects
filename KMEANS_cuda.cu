3c8a7f80f2299a2e98093e87a271db059c14fef7
ironmask2003 <lachithaperera75@gmail.com>
2025-02-25 01:37:28 -0800

    BlockSize modified

--- KMEANS_cuda.cu
+++ KMEANS_cuda.cu
@@ -438,7 +438,7 @@ int main(int argc, char* argv[])
     CHECK_CUDA_CALL( cudaMemcpy(d_distCentroids, distCentroids, K*sizeof(float), cudaMemcpyHostToDevice) );
 
     // Set of the grid and block dimensions
-    dim3 blockSize(256);
+    dim3 blockSize(1024);
     dim3 numBlocks((lines + blockSize.x - 1) / blockSize.x);
 
     dim3 numBlocks2((K + blockSize.x - 1) / blockSize.x);
