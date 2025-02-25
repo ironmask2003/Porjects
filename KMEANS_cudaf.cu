4366c9ed0a010114a513db9bf4df37b569654808
ironmask2003 <lachithaperera75@gmail.com>
2025-02-24 16:19:09 -0800

    Final v

--- KMEANS_cudaf.cu
+++ KMEANS_cudaf.cu
@@ -285,7 +285,9 @@ __global__ void max(float* d_centroids, float* d_auxCentroids, float* d_maxDist,
 
     if (id < d_K){
         d_distCentroids[id]=d_euclideanDistance(&d_centroids[id*d_samples], &d_auxCentroids[id*d_samples], d_samples);
-		atomicMax(d_maxDist, d_distCentroids[id]);
+		if(d_distCentroids[i]>d_maxDist) {
+            d_maxDist=d_distCentroids[i];
+        }
     }
 }
 
